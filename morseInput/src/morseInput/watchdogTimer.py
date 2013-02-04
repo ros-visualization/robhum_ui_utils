26c35ad9a64841ddb2c046e470aba6955ee97eb8
Andreas Paepcke <paepcke@cs.stanford.edu>
2013-02-04 10:30:06 -0800

    Safety commit

--- morseInput/src/morseInput/watchdogTimer.py
+++ morseInput/src/morseInput/watchdogTimer.py
@@ -7,19 +7,21 @@ from python_qt_binding import QtCore, QtGui;
 from QtCore import QTimer;
 from QtGui import QApplication;
 
-class WatchdogTimer(object):
+class WatchdogTimer(QTimer):
     
     def __init__(self, timeout=1000, callback=None, callbackArg=None, selfTest=False):
-        self.timeout = timeout
+        super(WatchdogTimer,self).__init__();
+        # Use leading underscore to avoid
+        # overwriting parent's 'timeout' signal:
+        self._timeout = timeout
         self.callback = callback
         self.callbackArg = callbackArg
 
         self.timedout = True;
         self.stopped  = True;
         
-        self.timer = QTimer();
-        self.timer.setSingleShot(True);
-        self.timer.timeout.connect(self.timerExpiredHandler);
+        self.setSingleShot(True);
+        self.timeout.connect(self.timerExpiredHandler);
                 
         if selfTest:
             self.runSelfTest();
@@ -29,20 +31,20 @@ class WatchdogTimer(object):
     def timedOut(self):
         return self.timedout;
 
-    def kick(self, timeout=None):
+    def kick(self, _timeout=None):
         self.timedout = False;
         self.stopped  = False;
-        if timeout is None:
-            timeout = self.timeout;
-        self.timer.start(timeout);
+        if _timeout is None:
+            _timeout = self._timeout;
+        self.start(_timeout);
         
     def stop(self):
-        self.timer.stop();
+        super(WatchdogTimer, self).stop();
         self.timedout = False;
         self.stopped  = True;
     
     def changeTimeout(self, newTimeout):
-        self.timeout = newTimeout;
+        self._timeout = newTimeout;
         
     def changeCallback(self, newCallback):
         self.callback = newCallback;
@@ -68,7 +70,15 @@ class WatchdogTimer(object):
                             
     def testTimeoutHandler(self):
         print("Timed out.");
-            
+        self.kick();
+        print("Stop test: stopped == %s" % self.stopped);
+        self.kick();
+        print("After kick: stopped == %s" % self.stopped);
+        self.stop();
+        if self.stopped:
+            print("Timer successfully stopped.");
+        else:
+            print("Bad: timer not stopped.");
     
 if __name__ == '__main__':
     
